3e155f016f96296b6cba59b055c76a1a17b8e7f5
pablopsn <pabloprina@hotmail.com>
2022-09-18 11:23:51 -0300

    TAREA 11

---
+++ script/script.js
@@ -0,0 +1,41 @@
+let diaSemana = prompt("¿Qué día es hoy?");
+switch (diaSemana) {
+case "lunes":
+
+alert("Lunes: 2hs de natación.");
+
+break;
+case "martes":
+
+alert("Martes: correr 5 km.");
+
+break;
+case "miercoles":
+
+alert("Miércoles: caminar 10 km.");
+
+break;
+case "jueves":
+
+alert("Jueves: 90 minutos de Padel.");
+
+break;
+case "viernes":
+
+alert("Viernes: práctica con el profesor");
+
+break;
+case "sabado":
+
+alert("Sábado: Hoy torneo");
+
+break;
+case "domingo":
+
+alert("Domingo: descanso");
+
+break;
+default:
+
+alert("Escribe un día de la semana en minúsculas.");
+}
---
+++ script/script2.js
@@ -0,0 +1,5 @@
+
+    var apariciones = prompt("¿Cuantas veces quieres que aparezca la frase HOLA MUNDO?");
+    for(i=0;i<apariciones;i++){
+             document.write("HOLA MUNDO" + i + "<br>");
+    }
